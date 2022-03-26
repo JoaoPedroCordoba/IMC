2451509a749f910025d27f50191dd3582be24798
JoaoPedroCordoba <jp.8pedro86@gmail.com>
2022-03-25 21:18:22 -0300

    finalizadoexercicio

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿double m ,kg, IMC;
+
+Console.WriteLine("Digite a Altura (m)..:");
+m = Convert.ToDouble(Console.ReadLine()!);
+
+Console.WriteLine("Digite o seu peso (kg))..:");
+kg = Convert.ToDouble(Console.ReadLine()!);
+
+IMC = (kg / (Math.Pow(m,2)));
+
+Console.WriteLine($"IMC:{IMC}kg/m²");
